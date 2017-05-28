7f4b30a8f66bd5caee126b0a84648c6f11e84b85
Ilyasyasin072 <ilyasyasin072@gmail.com>
2017-05-28 10:30:01 +0700

    Update Pertemuan5.py
    install Goslate
    
    $ pip install goslate

--- Pertemuan5/Pertemuan5.py
+++ Pertemuan5/Pertemuan5.py
@@ -4,6 +4,7 @@ print('Pilih Bahasa :')
 print('1.Indonesia Ke Inggris')
 print('2.Inggris Ke Indonesia')
 a = raw_input("Pilihan : ")
+
 if a == '1'    : kata = raw_input("Masukan : ")
 print(gs.translate(kata, 'id'))
 if a == '2'    : kata = raw_input("Enter : ")
